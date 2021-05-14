7e2c95a9551a3f979f28ebbfbcadcd5242029874
Holy One <>
2021-05-14 23:04:09 +0000

    Added a number program here ;)

--- Main.java
+++ Main.java
@@ -1,13 +1,40 @@
 public class Hello {
 
     public static void main(String[] args) {
+        // Day 1
+
         // Printing Hello World to Console
-        System.out.println("Hello World!");
+        System.out.println("Hello World!\nWe will be testing out some things in Java");
+
+        // Assinging Integers
 
         // Assinging Integer 5 to My First Number
         int myFirstNumber = 5;
+        //Assinging Interger 15 to My Second Number
+        int mySecondNumber = 5 + 10;
+        // Assinging Interger 6 to my Third Number
+        int myThirdNumber = 6;
 
+
+
+        // Printing
+
+        // Informing the users
+        System.out.println("Numbers: \n");
         // Printing myFirstNumber to Console
         System.out.println(myFirstNumber);
+        // Printing my second number to Console
+        System.out.println(mySecondNumber);
+        // Printing my third number to Console
+        System.out.println(myThirdNumber);
+        System.out.println("\nThree Numbers");
+        System.out.println("\n\nWe shall add them up!");
+
+        // My Total
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        // Printing My Total to Console
+        System.out.println("The total when we add up the three numbers is: \n");
+        System.out.println(myTotal);
+
     }
 }

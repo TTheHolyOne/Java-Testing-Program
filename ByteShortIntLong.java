7971e1fc74b599e7ca6551ec6e556225c8217e9d
Holy One <>
2021-05-14 23:52:18 +0000

    added some more stuff

--- ByteShortIntLong.java
+++ ByteShortIntLong.java
@@ -1,5 +1,3 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -11,5 +9,23 @@ public class Main {
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
     }
 }
